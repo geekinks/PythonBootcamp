853c3d9de8d309690fbc4668aa9c33bd6a93c86d
Maryaamah <maryamibrahimkoli@gmail.com>
2026-08-29 11:54:55 +0300

    Exercise1

---
+++ task/Maryaamah/student_result_checker.py
@@ -0,0 +1,37 @@
+print("==========================Student Result Checker================")
+
+name = (input "Enter your name: ")
+age = int(input "Enter your age: ")
+department = (input "Enter your department: ")
+
+English = (int(input "Enter your English marks: "))
+Maths = (int(input "Enter your Maths marks: "))
+Science = (int(input "Enter your Science marks: "))
+civics = (int(input "Enter your Civics marks: "))
+agriculture = (int(input "Enter your Agriculture marks: "))
+
+total_marks = English + Maths + Science + civics + agriculture
+average_marks = total_marks / 5
+
+if average_marks >= 70:
+    grade = "A"
+elif average_marks >= 60:
+    grade = "B"
+elif average_marks >= 50:
+    grade = "C"
+elif average_marks >= 45:
+    grade = "D"
+elif average_marks >= 40:
+    grade = "E"
+else:   
+    grade = "F"
+if average_marks >= 40:
+    result = "Pass"     
+else:
+    result = "Fail"
+
+    
+
+
+
+
