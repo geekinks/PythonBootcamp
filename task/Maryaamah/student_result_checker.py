5529e2af36b40f2d82a923590e50749e2ec82b2e
Maryaamah <maryamibrahimkoli@gmail.com>
2026-08-29 11:33:25 +0300

    Exercise1

--- task/Maryaamah/student_result_checker.py
+++ task/Maryaamah/student_result_checker.py
@@ -1,14 +1,14 @@
-print("==========================Student Result Checker================")
+gitprint("==========================Student Result Checker================")
 
-name = (input "Enter your name: ")
-age = int(input "Enter your age: ")
-department = (input "Enter your department: ")
+name = input ("Enter your name: ")
+age = int (input ("Enter your age: "))
+department = input ("Enter your department: ")
 
-English = (int(input "Enter your English marks: "))
-Maths = (int(input "Enter your Maths marks: "))
-Science = (int(input "Enter your Science marks: "))
-civics = (int(input "Enter your Civics marks: "))
-agriculture = (int(input "Enter your Agriculture marks: "))
+English = int(input ("Enter your English marks: "))
+Maths = int(input ("Enter your Maths marks: "))
+Science = int(input ("Enter your Science marks: "))
+civics = int(input ("Enter your Civics marks: "))
+agriculture = int (input ("Enter your Agriculture marks: "))
 
 total_marks = English + Maths + Science + civics + agriculture
 average_marks = total_marks / 5
