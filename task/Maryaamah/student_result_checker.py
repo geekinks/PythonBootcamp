c5bee10efddd85fbd6166dae849abb0538e57cad
Maryaamah <maryamibrahimkoli@gmail.com>
2026-08-30 17:16:44 +0300

    sample

--- task/Maryaamah/student_result_checker.py
+++ task/Maryaamah/student_result_checker.py
@@ -10,31 +10,49 @@ Science = int(input ("Enter your Science marks: "))
 civics = int(input ("Enter your Civics marks: "))
 agriculture = int (input ("Enter your Agriculture marks: "))
 
+
 total_marks = English + Maths + Science + civics + agriculture
 average_marks = total_marks / 5
 
 if average_marks >= 70:
     grade = "A"
-    print(grade)
+    print(f"Grade: {grade}")
 elif average_marks >= 60:
     grade = "B"
+    print(f"Grade: {grade}")
 elif average_marks >= 50:
     grade = "C"
+    print(f"Grade: {grade}")
 elif average_marks >= 45:
     grade = "D"
+    print(f"Grade: {grade}")
 elif average_marks >= 40:
     grade = "E"
+    print(f"Grade: {grade}")
 else:   
     grade = "F"
+    print(f"Grade: {grade}")
 if average_marks >= 40:
     result = "Pass"     
 else:
     result = "Fail"
-    print(total_marks)
-    print(average_marks)
+print("              ")
 
-    
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"department: {department}")
+print("              ")
 
+print(f"English: {English}")
+print(f"Maths: {Maths}")
+print(f"Science: {Science}")
+print(f"Civics: {civics}")
+print(f"Agriculture: {agriculture}")
+print("              ")
 
+print(f"Total Marks: {total_marks}")
+print(f"Average Marks: {average_marks}")
+print(f"Grade: {grade}")
+print(f"Result: {result}")
 
 
